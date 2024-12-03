541742681d66c7efced84d4ff2feef8cb88470d8
William Klausmeyer <willyk95@gmail.com>
2024-12-02 21:12:28 -0700

    Add more comments

--- runs.py
+++ runs.py
@@ -1,5 +1,9 @@
+# Import the optimizations library from Google
 from ortools.sat.python import cp_model
 
+# For each run, which runs are you able to get to from the bottom of it?
+# For each lift, which runs can you get to the top of from it
+# e.g. from the Catepillar lift you can get to Snowflake and Butterfly
 possible_runs = {
   'Pioneer': [
     'KC Cutoff',
@@ -207,21 +211,9 @@ all_runs.sort()
 #   print(run)
 # print(all_runs)
 
-# A class for printing the variables while searching for an optimal solution
-class VarArraySolutionPrinter(cp_model.CpSolverSolutionCallback):
-  """Print intermediate solutions."""
-
-  def __init__(self):
-    cp_model.CpSolverSolutionCallback.__init__(self)
-    self.__solution_count = 0
-
-  def on_solution_callback(self):
-    self.__solution_count += 1
-
-  def solution_count(self):
-    return self.__solution_count
-
 # Create all the possible runs
+# Passes in a run or lift, retrieves all the runs that that run can
+# lead to and adds it to the end of an array
 lift_runs = []
 def getNextRuns(base, last):
   global lift_runs
@@ -234,79 +226,113 @@ def getNextRuns(base, last):
     new_base.append(next_run)
     getNextRuns(new_base, next_run)
 
+# Starting with the lifts, build out the possible runs to get down
+# the mountain
 for lift in lifts:
   for start in possible_runs[lift]:
+    # start is the a run you can get to from the top of the lift
     base = [
       # lift,
       start,
     ]
     getNextRuns(base, start)
 
-# Create the modal
+# Initialize the modal
 model = cp_model.CpModel()
 
-# Create variables for each lift ride
+# This holds an array of run names to the lift rides it's a part of
+# boolean variables. i.e. if any of the lift rides in the array is used,
+# that run will be completed
 run_implications = {}
+
+# This list holds all of the possible list run boolean variables. It is
+# used to determine how many lift rides are needed to hit every ski run
 lift_run_bools = []
+
+# This holds the list of runs that have already been used so we don't get
+# duplicate ways down the mountain
 used_lift_runs = []
+
+# Create variables for each potential lift ride
 for lr in lift_runs:
+  # Some runs you can get to from multiple lifts, so make sure
+  # that we aren't re-using a potential way down
   lift_run_name = "_".join(lr)
   if lift_run_name in used_lift_runs:
     continue
   used_lift_runs.append(lift_run_name)
+
+  # Make a boolean variable. If true, this way down off the lift is used
   local_lift_run = model.NewBoolVar(lift_run_name)
+
+  # Save the boolean variable into a list that will be used to determine the
+  # number of times you need to ride the lift
   lift_run_bools.append(local_lift_run)
+
+  # Loop over each run in this way down the mountain
   for run in lr:
+    # If this run doesn't already have an entry in the run_implications object,
+    # create one as an empty list
     if run not in run_implications:
       run_implications[run] = []
+
+    # Add the current lift ride to the array
     run_implications[run].append(local_lift_run)
 
-# Create a variable for each run
-run_bools = {}
-run_ints = {}
+# This object has a key of a run name and then an integer containing how many
+# times the run will have been skied
 run_counts = {}
-# num_runs_done = model.NewIntVar(0, len(all_runs), "num_runs_done")
+# Loop over all of the run names
 for r in all_runs:
-  run_bools[r] = model.NewBoolVar(f"run_{r}")
+  # This says that at least one of the ways down the mountain associated with
+  # each run must be True. This will make sure that each run is skied at least
+  # once
   model.AddBoolOr(run_implications[r])
+  
+  # Create an integer variable that is the number of ways down the mountain that
+  # include this run are being used
   run_counts[r] = model.NewIntVar(0, 5, f"run_{r}")
   model.Add(run_counts[r] == sum(run_implications[r]))
+
+# Create a variable that counts how many repeated runs there are
 excess_runs = model.NewIntVar(0, len(all_runs * 10), "excess_runs")
 model.Add(excess_runs == sum([(run_counts[rc] - 1) * 2 for rc in run_counts]))
-# model.Add(num_runs_done == sum(run_bools[rb] for rb in run_bools))
-# model.Add(num_runs_done == len(all_runs))
 
-# Set the optimization to reduce the number of runs
+# Create a variable that counts the number of times that the lift will need to be ridden
 num_lift_rides = model.NewIntVar(0, len(lift_runs), "num_lift_runs")
 model.Add(num_lift_rides == sum(lift_run_bools))
 
+# Create an optimization variable (since we can only optimize to one) that is a sum
+# of the number of lift rides AND the number of extra runs
 optimize = model.NewIntVar(0, len(lift_runs) * 50, "optimized")
 model.Add(optimize == num_lift_rides + excess_runs)
 
+# Set the model to try to get the smallest number of the optimized variable
+# so the model will prefer solutions that have the lowest number of lift rides
+# plus the number of repeated runs
 model.Minimize(optimize)
-# model.Minimize(num_lift_rides)
-
-# Create the solution progress output class showing all the ratios
-solution_printer = VarArraySolutionPrinter()
 
 # Solve the problem
 solver = cp_model.CpSolver()
-status = solver.Solve(model, solution_printer)
+status = solver.Solve(model)
 
+# Make sure we have an optimal (or at least feasible) solution
 if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
+  # Number of times we need to ride the lifts
   print(f"Num lift rides: {solver.Value(num_lift_rides)}")
-  # print(f"Num runs: {solver.Value(num_runs_done)}")
   print("")
+  
+  # The different paths we need to take to get down the mountain in this solution
   print("Lift Rides:")
   for lrb in lift_run_bools:
     if solver.Value(lrb):
       print(lrb)
-
   print("")
+  
+  # Which runs will be repeated and how many times?
   print("Repeated Runs:")
   for run in run_counts:
     if solver.Value(run_counts[run]) > 1:
       print(run_counts[run], solver.Value(run_counts[run]))
 else:
   print("No optimal solution found")
-# print(len(all_runs))
